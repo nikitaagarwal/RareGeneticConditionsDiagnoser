c977d03ef5f53d5a9addae4d846788e8d862aaad
Nikita Agarwal <naa824@gmail.com>
2021-03-28 20:43:04 -0400

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,9 +1,10 @@
 import os
+
 from django.conf import settings
 from django.shortcuts import render
 from django.views.generic import TemplateView
+
 from app.forms import HomeForm
-from app.backend.preprocess import preprocess
 from app.backend.extraction import get_phenotypes, get_probable_diagnoses
 
 # Class-Based Views
@@ -19,17 +20,13 @@ class HomeView(TemplateView):
         form = HomeForm(request.POST)
 
         if form.is_valid():
-            # TODO(nikita): Save incoming requests in database.
-            # Clean to ensure there's no breakers/sql injection/hackery nonsense
+            # Clean to ensure there's no breakers/sql injection/hackery nonsense.
             text = form.cleaned_data['post']
             # Extract phenotypes from user input.
             input_phenotypes = get_phenotypes(text.lower())
-            # Once I take in the data, I don't want to see it in the input bar
+            # Once I take in the data, I don't want to see it in the input bar.
             # anymore, but I can show it below.
             form = HomeForm()
-            # Get the data dict serialized in JSON, if it exists. Else, create it.
-            data = preprocess()
-            results = get_probable_diagnoses(data, input_phenotypes)
-  
+            results = get_probable_diagnoses(input_phenotypes)
         args = {'form': form, 'text': text, 'results' : results, 'results_empty' : (len(results)==0)}
         return render(request, self.template_name, args)
